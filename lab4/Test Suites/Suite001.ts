d24374b329277fca4f5045056ec4ac0baabe64ca
Hong Dieu <hongdieupvt2000@gmail.com>
2020-10-08 21:48:03 +0700

    lab4

---
+++ lab4/Test Suites/Suite001.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6b97b3d7-6c9c-4399-abd4-dd616ba8eb4d</testSuiteGuid>
+   <testCaseLink>
+      <guid>2c868b84-e980-406e-911d-4d14d0cad58a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC001_profiles</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
